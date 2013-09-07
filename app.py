dd6ed66e5f423065f6169f94ef4c21fce8a2f5d4
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-09-07 12:28:21 -0400

    Implement users joining sessions

--- app.py
+++ app.py
@@ -14,17 +14,33 @@ def main_page():
 @app.route('/session', methods=['POST', 'GET'])
 def session():
     """Responds to session view requests"""
-    return render_template('session.html')
+    session_id = request.form['session_id']
+    user_dict = db_utils.get_user_dict_by_session_id(session_id)
+    users = user_dict.keys()
+    contributions = session_functions.get_public_contributions(users)
+    for user in users:
+        # Figure out how many contributions since the beginning of the competition.
+        user_dict[user] = contributions - user_dict[user]
+    return render_template('session.html', session_id=session_id, user_dict=user_dict)
 
 @app.route('/create', methods=['POST'])
 def create_session():
     """Creates a session, and inserts the data into the database."""
-    users = request.form['users']
-    contributions = get_contributions.get_public_contributions(users)
-    user_dict = session_functions.build_user_dict(users, contributions)
+    user = request.form['user']
+    contributions = get_contributions.get_public_contributions([user])
+    user_dict = session_functions.build_user_dict([user], contributions)
     session_id = session_functions.create_session_id()
-    mongoFunctions.insert_into_db(session_id, user_dict)
+    db_utils.insert_into_db(session_id, user_dict)
     return render_template('session.html', session_id=session_id)
+
+@app.route('/join', methods=['POST'])
+def join_session():
+    user = request.form['user']
+    contributions = get_contributions.get_public_contributions([user])
+    user_dict = db_utils.get_user_dict_by_session_id(session_id)
+    user_dict[user] = contributions
+    db_utils.update_user_dict(session_id, user_dict)
+    return render_template('session.html', session_id=session_id, user_dict=user_dict)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- db_utils.py
+++ db_utils.py
@@ -12,11 +12,18 @@ def insert_into_db(session_id, user_dict):
         session_id: The id of the session
         user_dict: a dictionary associating Github usernames to their original number of commits
     """
-	session = {"id": session_id,
+	session = {"session_id": session_id,
 				"user_dict": user_dict
 				}
 	#Only insert if the session is not already in the collection
-	if collection.find({"id": session_id}).count() == 0 and collection.find({"email": email}).count() == 0:
+	if collection.find({"session_id": session_id}).count() == 0 and collection.find({"email": email}).count() == 0:
 		collection.insert(session)
     else:
         #TODO(Sam): Generate a new session here
+
+def get_user_dict_by_session_id(session_id):
+    """
+    Retrieves the user dict associated with the given session id.
+    """
+
+    return db.find_one({'session_id': session_id})['user_dict']
